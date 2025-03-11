cb48eb5c1afe29ccc352985c67f416d14df83208
tamastorok <torok.tomi@gmail.com>
2025-03-11 09:00:33 +0100

    Frame fix 2

--- src/app/frames/compliment/page.tsx
+++ src/app/frames/compliment/page.tsx
@@ -3,30 +3,22 @@ import Demo from "~/components/Demo";
 
 const appUrl = process.env.NEXT_PUBLIC_URL;
 
-const frame = {
-  version: "next",
-  imageUrl: `${appUrl}/frames/compliment/opengraph-image`,
-  buttons: [
-    {
-      label: "Start",
-      action: "post",
-      target: `${appUrl}/frames/compliment`,
-    },
-  ],
-};
-
 export const metadata: Metadata = {
   title: "GLOW - Send anonymous compliments",
   description: "Send anonymous compliments to anyone on Warpcast",
   openGraph: {
     title: "GLOW - Send anonymous compliments",
     description: "Send anonymous compliments to anyone on Warpcast",
+    images: [`${appUrl}/frames/compliment/opengraph-image`],
   },
   other: {
-    "fc:frame": JSON.stringify(frame),
+    "fc:frame": "vNext",
+    "fc:frame:image": `${appUrl}/frames/compliment/opengraph-image`,
+    "fc:frame:post_url": `${appUrl}/frames/compliment`,
+    "fc:frame:button:1": "Start",
   },
 };
 
 export default function ComplimentFrame() {
-  return <Demo/>;
+  return <Demo title="GLOW - Send anonymous compliments" />;
 } 
