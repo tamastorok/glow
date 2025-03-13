cf63a38f125777bbc8e67a2242314b0313273e5e
tamastorok <torok.tomi@gmail.com>
2025-03-13 10:50:37 +0100

    openimag change

--- src/app/frames/compliment/opengraph-image.tsx
+++ src/app/frames/compliment/opengraph-image.tsx
@@ -13,12 +13,8 @@ export const contentType = "image/png";
 export default async function Image() {
   return new ImageResponse(
     (
-      <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-        <div tw="flex flex-col items-center justify-center">
-          <h1 tw="text-6xl font-bold mb-4">GLOW</h1>
-          <img src="/icon.png" alt="GLOW" tw="w-16 h-16 mb-4" />
-          <p tw="text-2xl text-gray-600">Send anonymous compliments</p>
-        </div>
+      <div tw="h-full w-full flex flex-col justify-center items-center relative">
+        <img src="/glow_frame.png" alt="GLOW" tw="w-full h-full" />
       </div>
     ),
     {
