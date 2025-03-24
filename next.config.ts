a79c5071d88b0dabf56bb063bab1bc0891ef1043
tamastorok <torok.tomi@gmail.com>
2025-03-24 15:49:50 +0100

    frame json fix 2

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,23 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['imagedelivery.net', 'i.imgur.com','avatars.steamstatic.com','lh3.googleusercontent.com','openseauserdata.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/farcaster/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
