d08b1fb2bcf7caba6aa6799dffd71cda97248b1c
tamastorok <torok.tomi@gmail.com>
2025-03-25 23:49:06 +0100

    next cf update2

--- next.config.ts
+++ next.config.ts
@@ -5,38 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['imagedelivery.net', 'i.imgur.com','avatars.steamstatic.com','lh3.googleusercontent.com','openseauserdata.com'],
   },
-  async headers() {
-    return [
-      {
-        source: '/.well-known/farcaster/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Content-Type',
-            value: 'application/json',
-          },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.useglow.xyz',
-          },
-        ],
-        destination: 'https://useglow.xyz/:path*',
-        permanent: true,
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
